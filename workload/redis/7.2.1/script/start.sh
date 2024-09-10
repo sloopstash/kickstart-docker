b3a6ad6b0722acf2f812461e5cdb797f5ec0ce93
Karthikeyan Chandramouli <karthik2502@gmail.com>
2024-09-10 17:36:02 +0000

    Updates
      -  Naming convention has been updated from IP to Service name for all the redis nodes
      -  Start script has been updated with th service name
      -  Cluster folder created and server.conf has been placed with the configuration cluster enabled yes

--- workload/redis/7.2.1/script/start.sh
+++ workload/redis/7.2.1/script/start.sh
@@ -12,12 +12,12 @@ log() {
 log "Starting Redis cluster initialization..."
 
 # Wait for all Redis nodes to be ready
-while ! (redis-cli -h 16.1.1.2 -p 3000 ping && \
-         redis-cli -h 16.1.1.3 -p 3000 ping && \
-         redis-cli -h 16.1.1.4 -p 3000 ping && \
-         redis-cli -h 16.1.1.5 -p 3000 ping && \
-         redis-cli -h 16.1.1.6 -p 3000 ping && \
-         redis-cli -h 16.1.1.7 -p 3000 ping); do
+while ! (redis-cli -h redis-1 -p 3000 ping && \
+         redis-cli -h redis-2 -p 3000 ping && \
+         redis-cli -h redis-3 -p 3000 ping && \
+         redis-cli -h redis-4 -p 3000 ping && \
+         redis-cli -h redis-5 -p 3000 ping && \
+         redis-cli -h redis-6 -p 3000 ping); do
   log "Waiting for Redis nodes to be ready..."
   sleep 5
 done
@@ -26,7 +26,7 @@ log "All Redis nodes are ready. Creating cluster..."
 
 # Create the Redis cluster
 {
-  yes yes | redis-cli --cluster create 16.1.1.2:3000 16.1.1.3:3000 16.1.1.4:3000 16.1.1.5:3000 16.1.1.6:3000 16.1.1.7:3000 --cluster-replicas 1
+  yes yes | redis-cli --cluster create redis-1:3000 redis-2:3000 redis-3:3000 redis-4:3000 redis-5:3000 redis-6:3000 --cluster-replicas 1
 } 2>&1 | tee -a $LOGFILE
 
 log "Redis cluster creation complete."
@@ -38,3 +38,4 @@ sleep 20
 redis-server --port 3000 --cluster-enabled yes --cluster-config-file nodes.conf --cluster-node-timeout 5000 --appendonly yes 2>&1 | tee -a $LOGFILE
 
 log "Redis server started with cluster enabled."
+
