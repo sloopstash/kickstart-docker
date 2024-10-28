3052430302c74b8737f59987c01aac93fc37ab5b
Sivabalan K <sivabtechit25@gmail.com>
2024-10-28 15:32:35 +0530

    Add script file for mongo-db replica cluster

--- workload/mongo-db/4.4.4/replica-cluster/script/init.js
+++ workload/mongo-db/4.4.4/replica-cluster/script/init.js
@@ -1,21 +1,29 @@
-// Initialize MongoDB replica cluster.
-var client = new Mongo('mongo-db-1:7000');
-var database = client.getDB('admin');
-var status = null;
 try {
-  status = rs.status();
-  print('MongoDB replica cluster already initialized.');
-} catch(exception) {
-  if(exception['codeName']=='NotYetInitialized') {
-    print('Initializing MongoDB replica cluster...');
-    rs.initiate({
-      '_id':'main',
-      'members':[
-        {'_id':1,'host':'mongo-db-1:7000'},
-        {'_id':2,'host':'mongo-db-2:7000'},
-        {'_id':3,'host':'mongo-db-3:7000'}
-      ]
-    });
-    print('MongoDB replica cluster initialized.');
-  }
+    var status = rs.status();
+    printjson(status);
+    print("Replica set is already created.");
+} catch (e) {
+    if (e.codeName === "NotYetInitialized") {
+        // The replica set is not yet initiated
+        print("Replica set is not yet initiated. Initiating now...");
+
+        var rsConfig = {
+            _id: "main",
+            members: [
+                { _id: 1, host: "mongo-db-1:7000" },
+                { _id: 2, host: "mongo-db-2:7000" },
+                { _id: 3, host: "mongo-db-3:7000" }
+            ]
+        };
+
+        // Initiate the replica set
+        rs.initiate(rsConfig);
+
+        // Confirm replica set initiation
+        var newStatus = rs.status();
+        printjson(newStatus);
+    } else {
+        // Other errors
+        print("An unexpected error occurred: " + e);
+    }
 }
