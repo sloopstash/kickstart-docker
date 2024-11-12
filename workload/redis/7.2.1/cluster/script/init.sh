20a94bdbc24ed1eeea48e4b741e420fc3e2fcbb6
Swetha Jagadeesh <142427797+SwethaJeevaprakash@users.noreply.github.com>
2024-11-12 14:40:28 +0530

    Update Redis configurations and initialization scripts (#50)
    Modify main.yml for Redis 7.2.1 cluster configuration

--- workload/redis/7.2.1/cluster/script/init.sh
+++ workload/redis/7.2.1/cluster/script/init.sh
@@ -1,41 +1,23 @@
 #!/bin/bash
 
-LOGFILE="/opt/redis/log/redis-cluster-init.log"
-
-touch $LOGFILE
-
-# Function to log messages
-log() {
-  echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" | tee -a $LOGFILE
+function wait_for_redis_node() {
+  local node=$1;
+  until redis-cli -h $node -p 3000 ping > /dev/null 2>&1; do
+    printf "Waiting for $node...\n";
+    sleep 5;
+  done;
+  printf "$node is ready.\n";
 }
 
-log "Starting Redis cluster initialization..."
+function create_redis_cluster() {
+  yes yes | redis-cli --cluster create redis-{1..6}:3000 --cluster-replicas 1;
+  printf "Redis cluster started.\n";
+}
 
-# Wait for all Redis nodes to be ready
-while ! (redis-cli -h redis-1 -p 3000 ping && \
-         redis-cli -h redis-2 -p 3000 ping && \
-         redis-cli -h redis-3 -p 3000 ping && \
-         redis-cli -h redis-4 -p 3000 ping && \
-         redis-cli -h redis-5 -p 3000 ping && \
-         redis-cli -h redis-6 -p 3000 ping); do
-  log "Waiting for Redis nodes to be ready..."
-  sleep 5
-done
+printf "Starting Redis cluster initialization...\n";
 
-log "All Redis nodes are ready. Creating cluster..."
+for node in redis-{1..6}; do
+  wait_for_redis_node $node;
+done;
 
-# Create the Redis cluster
-{
-  yes yes | redis-cli --cluster create redis-1:3000 redis-2:3000 redis-3:3000 redis-4:3000 redis-5:3000 redis-6:3000 --cluster-replicas 1
-} 2>&1 | tee -a $LOGFILE
-
-log "Redis cluster creation complete."
-
-# Waiting for cluster creation:
-sleep 20
-
-# Start Redis server with cluster enabled
-redis-server --port 3000 --cluster-enabled yes --cluster-config-file nodes.conf --cluster-node-timeout 5000 --appendonly yes 2>&1 | tee -a $LOGFILE
-
-log "Redis server started with cluster enabled."
-
+create_redis_cluster;
